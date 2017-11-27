b434f189683b9a94409286802521e09f33df30bd
Jervis <jervismuindi@yahoo.co.uk>
2017-11-26 21:52:15 -0800

    Enable go race detector

--- raft-cli/node1.sh
+++ raft-cli/node1.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 # Start node 1 of cluster.
-go run main.go  -nodes=localhost:50051,localhost:50052,localhost:50053
+go run -race main.go  -nodes=localhost:50051,localhost:50052,localhost:50053
--- raft-cli/node3.sh
+++ raft-cli/node3.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 # Start up node 3 of the cluster.
-go run main.go  -nodes=localhost:50053,localhost:50052,localhost:50051
+go run -race main.go  -nodes=localhost:50053,localhost:50052,localhost:50051
