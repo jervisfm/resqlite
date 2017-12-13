f536f813cbb92b7acde8fabbd9e000e7bb5a08c3
Jervis <jervismuindi@yahoo.co.uk>
2017-12-13 00:10:05 -0800

    Look for correct raft node so that we kill the process that binds to port'

--- raft-cli/stop_raft_node_cluster.sh
+++ raft-cli/stop_raft_node_cluster.sh
@@ -3,10 +3,8 @@
 # Stops any all running raft cluster nodes.
 
 echo "Looking for raft cluster nodes ..."
-ps | grep main.go | grep nodes
-
+ps | grep go | grep "main -nodes"
 
 echo "Stopping all nodes ..."
-ps | grep main.go | grep nodes | awk '{print $1}' | xargs kill -15
-
+ps | grep go | grep "main -nodes" | awk '{print $1}' | xargs kill -15
 echo "Done"
